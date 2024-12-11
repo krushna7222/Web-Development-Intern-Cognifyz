4ba515034ac84edcce22dd8129c3efc40f749cca
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2024-12-11 20:02:29 +0530

    Script for toggle button using dark mode activate

--- Level-2/Task-4/script.js
+++ Level-2/Task-4/script.js
@@ -4,12 +4,11 @@ var foot = document.getElementsByTagName("footer");
 var card = document.getElementsByClassName("card");
 
 // Toggle dark mode
-toggleCheckbox.addEventListener("change", () => {
+toggleCheckbox.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode", toggleCheckbox.checked);
   head[0].style.backgroundColor = toggleCheckbox.checked ? "#151515" : "#fff";
   foot[0].style.backgroundColor = toggleCheckbox.checked ? "#151515" : null;
-  Array.from(card).forEach(
-    (cards) =>
-      (cards.style.backgroundColor = toggleCheckbox.checked ? "black" : null)
+  Array.from(card).forEach((cards) =>
+    (cards.style.backgroundColor = toggleCheckbox.checked ? "black" : null)()
   );
 });
